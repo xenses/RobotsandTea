3693d9da803bd66472bf85444ecf8e0588df36f8
Laura C <codegrl@gmail.com>
2013-03-28 10:55:50 -0400

    Added Pelican blog generator and themes

---
+++ blag/pelicanconf.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'xenses'
+SITENAME = u'xenses the great'
+SITEURL = 'http://www.robotsandtea.com'
+
+TIMEZONE = 'America/New_York'
+
+THEME = "pelican-themes/tuxlite_tbs"
+DEFAULT_LANG = u'en'
+
+# Blogroll
+LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+          ('Python.org', 'http://python.org'),
+          ('Jinja2', 'http://jinja.pocoo.org'),)
+
+# Social widget
+SOCIAL = (('Twitter', 'http://twitter.com/xensesthegreat'),
+          ('PyLadiesATL', 'http://meetup.com/pyladiesatl'),
+          ('PyAtl', 'http://meetup.com/pyatl'),)
+
+DEFAULT_PAGINATION = 5
